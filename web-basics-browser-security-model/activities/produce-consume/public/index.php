f408752e58b3a790b0fc0160049c9705f5f89a7e
Mocanu Viorel Gabriel <viorel.mocanu@stud.acs.upb.ro>
2022-07-13 22:04:00 +0300

    Add public files for produce-consume challenge (#23)
    Signed-off-by: Gabriel Mocanu <gabi.mocanu98@gmail.com>

---
+++ web-basics-browser-security-model/activities/produce-consume/public/index.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+	<head>
+		<title>This is the title of the webpage!</title>
+	</head>
+	<body>
+		<p><a href="produce.php">Here</a> we produce.</p>
+		<p><a href="consume.php">Here</a> we consume.</p>
+	</body>
+</html>
