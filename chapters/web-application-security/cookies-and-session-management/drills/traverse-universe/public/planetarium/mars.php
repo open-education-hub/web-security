58b6ce57dc527d8956e8896a42738230129260d0
Gabriel Mocanu <gabi.mocanu98@gmail.com>
2023-06-30 16:06:54 +0300

    task: Add traverse-universe source code and deployment
    Signed-off-by: Gabriel Mocanu <gabi.mocanu98@gmail.com>
    Signed-off-by: Larisa-Alexia Filip <larisafilip211@yahoo.com>

---
+++ chapters/web-application-security/cookies-and-session-management/drills/traverse-universe/public/planetarium/mars.php
@@ -0,0 +1,3 @@
+<?php
+echo '<img src="mars.jpg" style="max-width: 1000px; margin: 0 auto; display: block; max-height: 500px;" />'
+?>
---
+++ chapters/web-application-security/cookies-and-session-management/drills/traverse-universe/src/planetarium/venus.php
@@ -0,0 +1,3 @@
+<?php
+echo '<img src="venus.jpg" style="max-width: 1000px; margin: 0 auto; display: block; max-height: 500px;" />'
+?>
