8592e844f86c93aeb27b700dae6cf2579c84a107
Gabriel Mocanu <gabi.mocanu98@gmail.com>
2026-01-02 16:24:09 +0200

    chore(web-basics): update give-to-get challenge
    Signed-off-by: Gabriel Mocanu <gabi.mocanu98@gmail.com>

--- chapters/web-application-security/web-basics/drills/give-to-get/sol/solution.sh
+++ chapters/web-application-security/web-basics/drills/give-to-get/sol/solution.sh
@@ -6,7 +6,7 @@ then
     url='http://127.0.0.1:'$PORT
 elif [[ $1 == "remote" ]] && [[ -z $2 ]] 
 then
-    url='http://141.85.224.118:'$PORT
+    url='http://141.85.224.101:'$PORT
 else
     url=$1':'$2
 fi
