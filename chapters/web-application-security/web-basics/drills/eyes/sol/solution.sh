8bce5f83251048cdf1cb4c0e3fe3ad7e733aade6
Gabriel Mocanu <gabi.mocanu98@gmail.com>
2026-01-02 16:23:58 +0200

    chore(web-basics): update eyes challenge
    Signed-off-by: Gabriel Mocanu <gabi.mocanu98@gmail.com>

--- chapters/web-application-security/web-basics/drills/eyes/sol/solution.sh
+++ chapters/web-application-security/web-basics/drills/eyes/sol/solution.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 PORT=30002
 
-if [[ $1 == "local" ]]
-then
-    url='http://127.0.0.1:'$PORT
-elif [[ $1 == "remote" ]] && [[ -z $2 ]] 
-then
-    url='http://141.85.224.118:'$PORT
+if [[ $1 == "local" ]]; then
+  url='http://127.0.0.1:'$PORT
+elif [[ $1 == "remote" ]] && [[ -z $2 ]]; then
+  url='http://141.85.224.101:'$PORT
 else
-    url=$1':'$2
+  url=$1':'$2
 fi
 
 # Eyes
