c45c1f3a3d8e4c9e2214994dfeae6d9406f58b2c
Gabriel Mocanu <gabi.mocanu98@gmail.com>
2026-01-02 18:18:12 +0200

    chore(web-basisc): update gimme challenge solution
    Signed-off-by: Gabriel Mocanu <gabi.mocanu98@gmail.com>

--- chapters/web-application-security/web-basics/drills/gimme/sol/solution.sh
+++ chapters/web-application-security/web-basics/drills/gimme/sol/solution.sh
@@ -12,7 +12,7 @@ fi
 # Gimme
 echo "Start exploit for Gimme"
 url=$url'/gimme'
-data=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 35)
+data=$(LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 35)
 flag=$(curl -s -X POST -H "Content-Type: text/plain" --data $data $url)
 echo "Flag is $flag"
 echo "-------------------------"
