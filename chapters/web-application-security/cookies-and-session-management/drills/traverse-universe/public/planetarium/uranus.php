2fecb8dae878fe92814a0119f89765952d0e13c9
Gabriel Mocanu <gabi.mocanu98@gmail.com>
2026-01-02 13:49:35 +0200

    chore(cookies): update traverse-universe challenge
    Signed-off-by: Gabriel Mocanu <gabi.mocanu98@gmail.com>

--- chapters/web-application-security/cookies-and-session-management/drills/traverse-universe/public/planetarium/uranus.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo '<img src="uranus.jpg" style="max-width: 1000px; margin: 0 auto; display: block; max-height: 500px;" />'
-?>
--- chapters/web-application-security/cookies-and-session-management/drills/traverse-universe/public/planetarium/venus.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo '<img src="venus.jpg" style="max-width: 1000px; margin: 0 auto; display: block; max-height: 500px;" />'
-?>
