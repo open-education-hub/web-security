8cfe57a4f4351c6d3231d1f2f816385d5b96d9f7
Gabriel Mocanu <gabi.mocanu98@gmail.com>
2026-01-02 13:54:36 +0200

    chore(cookies): update beep-beep-boop challenge
    Signed-off-by: Gabriel Mocanu <gabi.mocanu98@gmail.com>

--- chapters/web-application-security/cookies-and-session-management/drills/beep-beep-boop/public/73656372657420666f72204153494d4f.php
+++
@@ -1,9 +0,0 @@
-<?php
-setcookie('robotType', 'HUMAN'); // 48756d616e
-
-if ($_COOKIE['robotType'] == 'ASIMOV') { // 4153494d4f
-	echo 'Congrats! You’ve proven to be worthy. <br />Here is your secret: SSS{We_w0rsh1p_1saac_As1m0v}';
-} else {
-	echo 'This is a secure area that can only be accessed by the most advanced humanoid robots.';
-}
-?>
