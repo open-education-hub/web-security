b6fce149472a42c282a0e60047dd8f40fa6b2237
Carina Deaconu <carina99br@gmail.com>
2026-03-29 23:22:30 +0200

    r: reorganize file locations

--- chapters/web-application-security/sql-injection/drills/blacklist/src/db_conn.php
+++ chapters/web-application-security/sql-injection/drills/blacklist/src/db_conn.php
@@ -4,11 +4,8 @@
   $password = 'root';
   $database = 'blacklist';
 
-  // Create connection
   $conn = new mysqli($servername, $username, $password, $database);
-
-  // Check connection
   if ($conn->connect_error) {
-      die("Unable to connect to MYSQL server");
+    die("Unable to connect to MYSQL server");
   }
 ?>
