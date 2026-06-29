763d2a39acd386f725a049da2f0d920af479f7a2
Andreea-Carina Deaconu <carina99br@gmail.com>
2026-06-29 19:37:36 +0200

    add inspect-me-pem challenge src
    Signed-off-by: Andreea-Carina Deaconu <carina99br@gmail.com>

---
+++ chapters/network-and-communication-security/securing-communication/drills/inspect-me-pem/src/generate_self_signed_cert.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+flag=$(cat ../flag)
+
+openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes \
+  -keyout example.key -out example.crt -extensions san -config \
+  <(echo "[req]";
+    echo distinguished_name=req;
+    echo "[san]";
+    echo subjectAltName=DNS:example.com,DNS:$flag,IP:10.0.0.1
+    ) \
+  -subj "/CN=example.com"
